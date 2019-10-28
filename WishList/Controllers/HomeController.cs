894d121d9bae0e04c44e7080762f36c882948c6d
Andreas Dellrud <andreas.dellrud@cloudnine.se>
2019-10-28 09:14:02 +0100

    Fix namespace typo

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace WhishList.Controllers
+namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController() { }
         public IActionResult Index()
         {
             return View("Index");
